565db3a33ba0a07f67f71c62f5140b6daf6f97a3
Drew Hudec <drew@basistheory.com>
2023-08-07 09:19:45 -0400

    Adding .zprofile

--- zsh/install.sh
+++ zsh/install.sh
@@ -3,4 +3,5 @@
 set -e
 
 ln -sf "${DOTFILES_LOCATION}/zsh/.zshrc" "${HOME}/.zshrc"
+ln -sf "${DOTFILES_LOCATION}/zsh/.zprofile" "${HOME}/.zprofile"
 ln -sf "${DOTFILES_LOCATION}/zsh/.p10k.zsh" "${HOME}/.p10k.zsh"
