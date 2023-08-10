58baee14eac5b31b88846fd54bcb2fd8c24acd30
Drew Hudec <drew@basistheory.com>
2023-08-10 15:20:43 -0400

    Adds EKS and k9s aliases

--- omz/aliases.zsh
+++ omz/aliases.zsh
@@ -1,5 +1,7 @@
 # AWS
 alias aws-login='aws sso login --sso-session BasisTheory'
+alias aws-eks-login-dev-us-east-2='aws eks update-kubeconfig --region us-east-2 --name basistheory-dev --profile Development'
+alias aws-eks-login-dev-us-west-2='aws eks update-kubeconfig --region us-west-2 --name basistheory-dev --profile Development'
 
 # Utilities
 alias uuid='uuidgen | tr "[:upper:]" "[:lower:]" | tr -d "\n" | pbcopy'
@@ -9,3 +11,7 @@ alias src='cd $HOME/src/github'
 # zsh
 alias zsh-edit='code $HOME/.zshrc'
 alias zsh-source='source $HOME/.zshrc'
+
+# k9s
+alias k9s-dev-us-east-2='k9s --context arn:aws:eks:us-east-2:914054469264:cluster/basistheory-dev'
+alias k9s-dev-us-west-2='k9s --context arn:aws:eks:us-west-2:914054469264:cluster/basistheory-dev'
