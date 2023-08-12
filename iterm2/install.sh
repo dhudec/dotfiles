9f69ede3afba8b024bc7225d0e238dfeeeee6389
Drew Hudec <drew@basistheory.com>
2023-08-12 17:12:34 -0400

    Adds .hushlogin file

--- iterm2/install.sh
+++ iterm2/install.sh
@@ -6,3 +6,5 @@ set -e
 defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "${DOTFILES_LOCATION}/iterm2"
 # Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+
+ln -sf "${DOTFILES_LOCATION}/iterm2/.hushlogin" "${HOME}/.hushlogin"
